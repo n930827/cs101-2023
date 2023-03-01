4ee4b21f72acf5658a6f31915c3605d71a9e676e
B1129029 <120006585+n930827@users.noreply.github.com>
2023-03-01 20:16:56 +0800

    Create main2.c

---
+++ HW 1/main2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[] ="A3B2C4A9";
+    int len = sizeof(a)-1;
+    char word;
+    int num;
+    for (int i=0;i<len;i++){
+        if (i%2){
+            num = a[i]-48;
+            for(int j=0;j<num;j++)
+            printf("%c",word);
+        }else{
+            word = a[i];
+        }
+    }
+    return 0;
+}
