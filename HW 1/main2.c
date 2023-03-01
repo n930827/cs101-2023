70fb6cb666b5368b9471e285ffad93867070d8e6
B1129029 <120006585+n930827@users.noreply.github.com>
2023-03-01 20:18:19 +0800

    Update main2.c

--- HW 1/main2.c
+++ HW 1/main2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main ()
 {
     char a[] ="A3B2C4A9";
     int len = sizeof(a)-1;
     char word;
     int num;
-    for (int i=0;i<len;i++){
-        if (i%2){
+    for (int i=0;i<len;i++) {
+        if (i%2) {
             num = a[i]-48;
-            for(int j=0;j<num;j++)
-            printf("%c",word);
-        }else{
+            for (int j=0;j<num;j++)
+            printf ("%c",word);
+        }else {
             word = a[i];
         }
     }
